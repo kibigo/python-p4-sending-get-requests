addd95d640059f710c05c4eb41b7edb5e3aba997
kibigo <jacksonkibigo99@gmail.com>
2023-11-22 21:03:20 +0300

    Completed Assignment

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,42 @@
+# import requests
+# import json
+
+# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+
+# response = requests.get(url)
+
+# json_content = json.loads(response.text)
+
+# print(json.dumps(json_content, indent=4))
+
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+class GetPrograms:
 
-response = requests.get(url)
+    def get_programs():
+        
+        URL = "https://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-json_content = json.loads(response.text)
+        response = requests.get(URL)
 
-print(json.dumps(json_content, indent=4))
+        return response.content
+    
+    def program_school(self):
+
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
+        
+
+# programs = GetPrograms.get_programs()
+# print(programs)
+
+programs = GetPrograms.get_programs()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print (school)
